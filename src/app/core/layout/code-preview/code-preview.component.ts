637ae402d67a6a2e7f5d52d05e54cb1eef14302f
uyen-luu <it.luudinhuyen@gmail.com>
2025-03-23 16:43:32 +0700

    Teamp

---
+++ src/app/core/layout/code-preview/code-preview.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code-preview',
+  imports: [],
+  templateUrl: './code-preview.component.html',
+  styleUrl: './code-preview.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CodePreviewComponent {
+
+}
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,85 +1,37 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  OnInit,
-} from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CodePreviewComponent } from '@app/core/layout/code-preview/code-preview.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, CodePreviewComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
-  dynamicText: string = '';
-  phrases: string[] = [
-    '.Net Developer.',
-    'Angular Developer.',
-    'Azure Developer',
-    'Fullstack Developer',
-  ];
-
-  quote!: string;
-
-  currentPhraseIndex: number = 0;
-  currentCharIndex: number = 0;
-  isDeleting: boolean = false;
-  typingSpeed: number = 100;
-  deletingSpeed: number = 100;
-  delayBetweenPhrases: number = 2000;
-
-  constructor(private titleService: Title, private _ref: ChangeDetectorRef) {
-    this.titleService.setTitle('UL | Home');
-  }
-
-  ngOnInit(): void {
-    this.getQuote();
-    this.type();
-  }
-
-  private calculateDiff(from: Date, to: Date) {
-    return Math.floor(
-      (Date.UTC(from.getFullYear(), from.getMonth(), from.getDate()) -
-        Date.UTC(to.getFullYear(), to.getMonth(), to.getDate())) /
-        (1000 * 60 * 60 * 24)
-    );
-  }
-  private getQuote() {
-    const startDate = new Date('2017-01-16');
-    const diff = this.calculateDiff(startDate, new Date());
-    this.quote = `I am a developer with ${Math.round(
-      Math.abs(diff / 365)
-    )} years experience in building software. I
-        leverage my fluency in English, college education, and coding skills to
-        craft accessible, scalable and high-performance applications.`;
-  }
-
-  type() {
-    const currentPhrase = this.phrases[this.currentPhraseIndex];
-    if (this.isDeleting) {
-      this.dynamicText = currentPhrase.substring(0, this.currentCharIndex - 1);
-      this.currentCharIndex--;
-    } else {
-      this.dynamicText = currentPhrase.substring(0, this.currentCharIndex + 1);
-      this.currentCharIndex++;
-    }
-
-    if (!this.isDeleting && this.currentCharIndex === currentPhrase.length) {
-      this.isDeleting = true;
-      setTimeout(() => this.type(), this.delayBetweenPhrases);
-    } else if (this.isDeleting && this.currentCharIndex === 0) {
-      this.isDeleting = false;
-      this.currentPhraseIndex =
-        (this.currentPhraseIndex + 1) % this.phrases.length;
-      setTimeout(() => this.type(), 500);
-    } else {
-      const speed = this.isDeleting ? this.deletingSpeed : this.typingSpeed;
-      setTimeout(() => this.type(), speed);
-    }
-    this._ref.detectChanges();
-  }
+  ngOnInit(): void {}
+  profile = {
+    name: 'Uyen Luu',
+    jobTitle: 'Full-stack Developer',
+    socials: [
+      {
+        uri: 'https://www.linkedin.com/in/uyen-luu/',
+        icon: 'fab fa-linkedin',
+      },
+      {
+        uri: 'https://github.com/uyen-luu',
+        icon: 'fab fa-github',
+      },
+      {
+        uri: 'mailto:it.luudinhuyen@gmail.com',
+        icon: 'fas fa-envelope',
+      },
+      {
+        uri: 'https://www.facebook.com/it.luudinhuyen/',
+        icon: 'fab fa-facebook',
+      },
+    ],
+  };
 }
