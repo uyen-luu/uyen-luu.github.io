d5700279cb4cea334d94577f6426d5ea0390fcfd
uyen-luu <it.luudinhuyen@gmail.com>
2025-03-24 20:01:02 +0700

    temp

--- src/app/features/experience/experience.component.ts
+++ src/app/features/experience/experience.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ExperienceComponent {
-
+  items = [
+    {
+      duration: '(Jan 2022 - Present)',
+      title: 'Software Engineer I',
+      description: 'Teton Private Ltd.',
+    },
+    {
+      duration: '(Jun 2021 - Jan 2022)',
+      title: 'FullStack Developer',
+      description: 'Fiverr (freelance)',
+    },
+    {
+      duration: '(Jan 2018 - Present)',
+      title: 'Self Employed',
+      description: 'Code and build something in everyday.',
+    },
+  ];
 }
