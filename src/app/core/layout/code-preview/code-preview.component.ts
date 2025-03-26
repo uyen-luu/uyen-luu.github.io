65c1d34c5ab1dd1aad4cfe092a7120543f8902cd
uyen-luu <it.luudinhuyen@gmail.com>
2025-03-26 21:47:16 +0700

    Update code preview

--- src/app/core/layout/code-preview/code-preview.component.ts
+++ src/app/core/layout/code-preview/code-preview.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-code-preview',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './code-preview.component.html',
   styleUrl: './code-preview.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CodePreviewComponent {
-
+  skills = [
+    'React',
+    'NextJS',
+    'Redux',
+    'Express',
+    'NestJS',
+    'MySql',
+    'MongoDB',
+    'Docker',
+    'AWS',
+  ];
+  booleanFields = {
+    hardWorker: true,
+    quickLearner: true,
+    problemSolver: true,
+  };
 }
