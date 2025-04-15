8871722e352128b3427f3f58de1256c925e0f002
uyen-luu <it.luudinhuyen@gmail.com>
2025-04-15 21:36:13 +0700

    Add scroll to top button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,22 @@
-import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {
   ContainerComponent,
   FooterComponent,
   HeaderComponent,
 } from '@app/core/layout';
+import { ScrollToTopComponent } from './core/layout/scroll-to-top/scroll-to-top.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [HeaderComponent, ContainerComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    ContainerComponent,
+    FooterComponent,
+    ScrollToTopComponent,
+  ],
 })
 export class AppComponent {
   title = 'ul-portfolio';
---
+++ src/app/core/layout/scroll-to-top/scroll-to-top.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-to-top',
+  imports: [CommonModule],
+  templateUrl: './scroll-to-top.component.html',
+  styleUrl: './scroll-to-top.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ScrollToTopComponent implements AfterViewInit {
+  isVisible = false;
+  readonly SCROLL_THRESHOLD = 50;
+  private scrollElement: HTMLElement | null = null;
+  constructor(private _ref: ChangeDetectorRef) {}
+  ngAfterViewInit(): void {
+    this.scrollElement = document.getElementById('scrollContainer');
+    if (this.scrollElement) {
+      this.scrollElement.addEventListener('scroll', this.onScroll.bind(this));
+    }
+  }
+
+  onScroll(): void {
+    if (this.scrollElement) {
+      const scrollTop = this.scrollElement.scrollTop;
+      this.isVisible = scrollTop > this.SCROLL_THRESHOLD;
+      this._ref.markForCheck();
+    }
+  }
+
+  scrollToTop(): void {
+    this.scrollElement?.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+}
